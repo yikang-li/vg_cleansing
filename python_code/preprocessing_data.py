db9f71d0ed55efc0d7d08b53c874e1a90c0e0a64
LI Yikang <allen.li.thu@gmail.com>
2017-02-16 11:13:45 +0800

    fix the bug when generating bbox from [x, y, w, h] to [x1, y1, x2, y2] and extract top-150 object and top-50 predicate

--- python_code/preprocessing_data.py
+++ python_code/preprocessing_data.py
@@ -65,11 +65,11 @@ for d_id,rs in enumerate(relationships_data):
             relationship_item['object'] = normalized_object
             relationship_item['subject'] = normalized_subject
             relationship_item['sub_box'] = \
-                    (r['subject']['x'], r['subject']['y'], r['subject']['x'] + r['subject']['w'], \
-                     r['subject']['y'] + r['subject']['h'])
+                    (r['subject']['x'], r['subject']['y'], r['subject']['x'] + r['subject']['w'] - 1, \
+                     r['subject']['y'] + r['subject']['h'] - 1)
             relationship_item['obj_box'] = \
-                    (r['object']['x'], r['object']['y'], r['object']['x'] + r['object']['w'], \
-                     r['object']['y'] + r['object']['h'])
+                    (r['object']['x'], r['object']['y'], r['object']['x'] + r['object']['w'] - 1, \
+                     r['object']['y'] + r['object']['h'] - 1)
             relationship_item['predicate'] = normalized_predicate
             if 'relationships' not in im_relationships.keys():
                 im_relationships['relationships'] = [relationship_item]
